8d68c956d4b7181978115e2d7cb3d474e34d08f0
lemur97 <lucka@hyncica.eu>
2023-08-30 20:19:01 +0200

    Update todo.php

--- todo.php
+++ todo.php
@@ -13,11 +13,13 @@ $sql_dotaz = "SELECT * FROM `Users` WHERE `ID`= $user_id";
 $result = mysqli_query($db, $sql_dotaz) or die($sql_dotaz);
 $row = $result->fetch_assoc();
 
-echo "Ahoj ".$row["username"];
+echo "Ahoj " . $row["username"];
 
-
-
-if ($_POST["complete"] == "true") {
+if ($_POST["submit"] == "true") {
+    $novy_ukol = $_POST["description"];
+    $sql_insert = "INSERT INTO `todos` (`statusID`, `todo`) VALUES (1,$novy_ukol)";
+    $resultinsert = mysqli_query($db, $sql_insert) or die($sql_insert);
+} elseif ($_POST["complete"] == "true") {
     $todoID = $_POST["id"];
     $sql_complete = "UPDATE `todos` SET `statusID`= 2 WHERE `ID`= $todoID";
     $resultcomplete = mysqli_query($db, $sql_complete) or die($sql_complete);
@@ -25,10 +27,6 @@ if ($_POST["complete"] == "true") {
     $todoID = $_POST["id"];
     $sql_delete = "DELETE FROM `todos` WHERE `ID`= $todoID";
     $resultdelete = mysqli_query($db, $sql_delete) or die($sql_delete);
-} elseif (isset ($_POST["submit"])) {
-    $novy_ukol = $_POST ["description"];
-    $sql_insert = "INSERT INTO `todos` (`statusID`, `todo`) VALUES ('1',$novy_ukol)";
-    $resulinsert = mysqli_query($db, $sql_insert) or die($sql_insert);
 }
 
 $sql_dotaz2 = "SELECT * FROM `todos` WHERE `userID`= $user_id";
@@ -36,32 +34,32 @@ $result2 = mysqli_query($db, $sql_dotaz2) or die($sql_dotaz2);
 ?>
 
 <form action="todo.php" method="POST">
-         <input type="text" name="description" placeholder="pridej ukol"> 
-         <input type="submit" name="submit" value="pridat">
-     </form>
+    <input type="text" name="description" placeholder="pridej ukol"> 
+    <input type="submit">
+</form>
 
 <table> 
     <thead><th>Task</th><th></th><th></th></thead>
 <tbody>
 
-    <?php while ($row = $result2->fetch_assoc()) { ?>
+<?php while ($row = $result2->fetch_assoc()) { ?>
         <tr>
             <td>
-                <?php echo $row["todo"]; ?>  
+    <?php echo $row["todo"]; ?>  
             </td>
             <td>
-                <?php if ($row["statusID"] == 1) { ?>
+    <?php if ($row["statusID"] == 1) { ?>
                     <form action="todo.php" method="POST">
                         <button type="submit" name="complete">Complete!</button>
                         <input type="hidden" name="id" value="<?= $row['ID'] ?>">
                         <input type="hidden" name="complete" value="true">
 
                     </form>  
-                    <?php
-                } else {
-                    echo "Task complete!";
-                }
-                ?>
+        <?php
+    } else {
+        echo "Task complete!";
+    }
+    ?>
             </td>
             <td>
                 <form action="todo.php" method="POST">
@@ -73,9 +71,9 @@ $result2 = mysqli_query($db, $sql_dotaz2) or die($sql_dotaz2);
             </td>
 
         </tr>    
-        <?php
-    }
-    ?>
+    <?php
+}
+?>
 </tbody>
 </table> 
 
